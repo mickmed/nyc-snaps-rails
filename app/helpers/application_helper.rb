c577af64476d8f75bae75734b024691b5b4a137d
mickmed <mick2090@gmail.com>
2018-06-18 13:03:57 -0400

    .gitignore update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
       # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Mick Medium"
+    base_title = "nyc snaps - mick medium"
     if page_title.empty?
       base_title
     else
