d03b12ca49e40d9d8210117fb73a14c1f5740a74
mickmed <mick2090@gmail.com>
2018-06-28 16:21:48 -0400

    updates

--- db/migrate/20180611211051_add_impressions_count_to_photo.rb
+++ db/migrate/20180611211051_add_impressions_count_to_photo.rb
@@ -1,5 +1,5 @@
 class AddImpressionsCountToPhoto < ActiveRecord::Migration[5.2]
   def change
-    add_column :photos, :impressions_count, :integer
+    add_column (:photos, :impressions_count, :integer)
   end
 end
