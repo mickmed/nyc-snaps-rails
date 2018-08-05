0a6af3475111409623b24471c2e9ea8bbcf4f5aa
mickmed <mick2090@gmail.com>
2018-08-05 00:19:08 -0400

    photos_count

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -24,6 +24,7 @@ class PhotosController < ApplicationController
       if @category == 'favorites'
         @photos = Photo.all.joins(:impressions).group('photos.id').order('count(photos.id) desc').limit(PHOTOS_PER_PAGE).offset((@page-1) * PHOTOS_PER_PAGE) 
         @photos_all_in_category = Photo.all.joins(:impressions).group('photos.id').order('count(photos.id) desc').limit(12)
+        
       end        
       
       if @category == 'newest'
@@ -35,7 +36,7 @@ class PhotosController < ApplicationController
         @photos = Photo.where(categories: {name: params[:category]}).includes(:categories).limit(PHOTOS_PER_PAGE).offset((@page-1) * PHOTOS_PER_PAGE) 
         @photos_all_in_category = Photo.where(categories: {name: params[:category]}).includes(:categories)
       end
-      
+      @photos_count = @photos_all_in_category
       @ogimage = @photos.shuffle[1]
       
       session[:photo_flick] = @photos_all_in_category
